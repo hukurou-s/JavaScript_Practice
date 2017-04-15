016985e7637293fd2396b359c3b36e4f50765bca
hukurou-s <s14t206@stmail.eng.kagawa-u.ac.jp>
2017-04-16 01:58:53 +0900

    横方向の勝利判定実装

--- practice/index.js
+++ practice/index.js
@@ -25,10 +25,24 @@ hogeFunc();
 	const array = _.map(pieces, (element) => element.className.replace('piece', '').trim());
 	return _.chunk(array, 3);
     };
-    
+
     const gameIsEnd = () => {
 	const array = generateMultipulArray();
 	console.table(array);
+
+	// 勝敗判定(横)
+	for ( var i = 0; i < array.length; i++ ) {    // 行の数だけループ
+	    const horizontalPieceColor = array[i][0];
+
+	    if ( !horizontalPieceColor ) { continue; }    // 行の数だけループ
+	    
+	    for ( var j = 1; j < array[i].length; j++ ) {
+		if ( array[i][j] !== horizontalPieceColor ) { break; }    // 色が同じでなければ次の列へ
+		if ( j === array[i].length-1 ) { return true; }    // for文が回りきっていたらtrue
+	    }
+	    
+	}
+
 	return false;
     };
     
